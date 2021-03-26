7f81b2f4cfef89a62c0559b39206b66958263809
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-03-26 22:21:27 +0300

    Add files via upload

---
+++ Task 2/out/outab.java
@@ -0,0 +1,10 @@
+//package com.max.idea;
+
+public class outab {
+    public static void main (String[] args){
+        byte numb = 127;
+        double numa = 3.1416;
+        System.out.println(numb);
+        System.out.print(numa);
+}
+}
