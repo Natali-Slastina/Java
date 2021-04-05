2e6d299ab806d71d0ad0c771027145b16ac8fe33
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-05 23:18:56 +0300

    Update Taskeight.java

--- Task 8/Taskeight.java
+++ Task 8/Taskeight.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Taskeight {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Choose  any integer number > 0");
+        System.out.println("Enter  any integer number > 0");
         int n = scan.nextInt();
         int sum = 0;
         if (n < 0 || n == 0)
