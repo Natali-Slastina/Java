110e6d2b072776e9c58eb4465b221ae257bb92da
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-05 12:33:30 +0300

    Update Taskseven2.java

--- Task 7/Taskseven2.java
+++ Task 7/Taskseven2.java
@@ -16,7 +16,6 @@ public class Taskseven {
             int size = scan.nextInt();
             double[] myArray = new double[size];
             if (size != 0) {
-
                 System.out.println("Enter items of your array");
                 for (int i = 0; i < size; i++) {
                     myArray[i] = scan.nextDouble();
@@ -32,8 +31,7 @@ public class Taskseven {
                     if (myArray[i] == x || myArray[i] == y || myArray[i] == z)
                         System.out.println("Your item " + myArray[i] + ". Данное значение имеется в константах");
                 }
-            }
-            else
+            } else
                 System.out.println("Your array []. Enter length not equal to zero");
         } else if (dim == 2) {
             System.out.println("Enter number of rows");
@@ -41,7 +39,7 @@ public class Taskseven {
             System.out.println("Enter number of columns");
             int cols = scan.nextInt();
             double[][] myArray = new double[rows][cols];
-            if (rows !=0 && cols != 0) {
+            if (rows != 0 && cols != 0) {
 
                 System.out.println("Enter items of your array. First the first line, etc");
                 for (int i = 0; i < rows; i++) {
@@ -59,8 +57,7 @@ public class Taskseven {
                             System.out.println("Your item " + myArray[i][j] + ". Данное значение имеется в константах");
                     }
                 }
-            }
-            else
+            } else
                 System.out.println("Enter number not equal to zero");
         } else
             System.out.println("You entered an invalid value");
