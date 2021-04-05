b9c4cd539e2b248bcd0b7e444007005f24e907a8
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-05 21:06:39 +0300

    Create taskeight.java

---
+++ Task 8/taskeight.java
@@ -0,0 +1,21 @@
+//package com.max.idea;
+
+import java.util.Scanner;
+
+public class Taskeight {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Choose  any integer number > 0");
+        int n = scan.nextInt();
+        int sum = 0;
+        if (n < 0 || n == 0)
+            System.out.println("You entered an invalid number");
+        else {
+            for (int i = 1; i < n; i++) {
+                if (i % 2 != 0)
+                    sum += i;
+            }
+            System.out.print(sum);
+        }
+    }
+}
