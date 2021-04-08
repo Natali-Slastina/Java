138385523879f9f190a2550d3b402276aa026327
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-09 00:03:41 +0300

    Update Tasktwel.java

--- Task 12/Tasktwel.java
+++ Task 12/Tasktwel.java
@@ -8,29 +8,26 @@ public class Tasktwel {
         //Напишите программу, принимающую на вход строку “I like Java!!!”
         System.out.println("Enter your string  \"I like Java!!!\"");
         String str = scan.nextLine();
-        if(str.equals("I like Java!!!") == true){
+        if(str.equals("I like Java!!!")){
             //Проверить, содержит ли строка подстроку “Java” (используйте contains()).
             boolean got = str.contains("Java");
             System.out.println("String contains \"Java\" : " + got);
             //Проверить, начинается ли строка с подстроки “I like”
-            boolean start;
-            start = str.startsWith("I like");
+            boolean start = str.startsWith("I like");
             System.out.println("String starts with \"I like\": " + start);
             //Проверить, заканчивается ли строка с подставки “!!!”
-            boolean ends;
-            ends = str.endsWith("!!!");
+            boolean ends = str.endsWith("!!!");
             System.out.println("String ends with \"!!!\": " + ends);
             //Если 3 предыдущих условия верны, выведите данную строку, преобразованную к верхнему регистру.
             if (got == true && start == true && ends == true) {
                 String result = str.toUpperCase();
                 System.out.println(result);
             }
-            //Замените все символы ‘a’ на ‘о’ и введите подстроку “Jovo” на экран
+            //Замените все символы "a" на "о" и введите подстроку "Jovo" на экран
             String rep = str.replace("a", "o");
             String result = rep.substring(7, 11);
             System.out.println(result);
         } else
             System.out.println("Your string is not equals \"I like Java!!!\"");
-
     }
 }
