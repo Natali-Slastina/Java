184127fc1c6a91755040b007c4791d0396d3d7ed
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-09 22:40:13 +0300

    Update Taskelev.java

--- Task 11/Taskelev.java
+++ Task 11/Taskelev.java
@@ -9,11 +9,13 @@ public class Taskelev {
         System.out.println("Enter your first value");
         String str = scan.nextLine();
         Double num1 = Double.valueOf(str);
-        //Ввести второе число с клавиатуры и сохранить его в целочисленную переменную типа int. ?возможность вводить дабл, но сохранять как инт?
+        //Ввести второе число с клавиатуры и сохранить его в целочисленную переменную типа int.
         System.out.println("Enter your second value");
-        Integer num2 = scan.nextInt();
+        int num2 = (int) scan.nextDouble();
         //Сравнить 2 числа и вывести большее на экран
         double num3 = num2;
+        System.out.println("Your second value is updated: " + num3);
+        //Сравнить 2 числа и вывести большее на экран
         int result = Double.compare(num3,num1);
         if (result == 0)
             System.out.println("Your numbers is equals");
@@ -21,7 +23,7 @@ public class Taskelev {
             System.out.println("Max is " + num3);
         else
             System.out.println("Max is " + num1);
-        //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double ?оба числа уже дабл?
+        //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double
         double result2 = Math.min(num1, num3);
         if (num1 != num3)
             System.out.println("Min is " + result2);
