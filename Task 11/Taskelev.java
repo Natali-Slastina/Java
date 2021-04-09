f9d601209eaf8dd69a8ebcdde84eb11cd97c874b
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-09 23:24:05 +0300

    Update Taskelev.java

--- Task 11/Taskelev.java
+++ Task 11/Taskelev.java
@@ -1,1 +1,30 @@
 
+//package com.max.idea;
+
+import java.util.Scanner;
+
+public class Taskelev {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //Ввести первое  число с клавиатуры и записать его в строковую переменную.
+        System.out.println("Enter your first value");
+        String str = scan.nextLine();
+        double num1 = Double.valueOf(str);
+        //Ввести второе число с клавиатуры и сохранить его в целочисленную переменную типа int.
+        System.out.println("Enter your second int value");
+        int num2 = scan.nextInt();
+        //Сравнить 2 числа и вывести большее на экран
+        double num3 = num2;
+        int result = Double.compare(num1,num3);
+        if (result == 0)
+            System.out.println("Your numbers is equals");
+        else if (result == 1)
+            System.out.println("Max is " + num1);
+        else
+            System.out.println("Max is " + num3);
+        //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double
+        double result2 = Math.min(num1, num3);
+        if (num1 != num3)
+            System.out.println("Min is " + result2);
+    }
+}
