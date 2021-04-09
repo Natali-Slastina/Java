ee650f5cd45adf4d87dead38e44d4c011afa94c4
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-09 23:38:57 +0300

    Update Taskelev.java

--- Task 11/Taskelev.java
+++ Task 11/Taskelev.java
@@ -15,14 +15,14 @@ public class Taskelev {
         int num2 = scan.nextInt();
         //Сравнить 2 числа и вывести большее на экран
         double num3 = num2;
-        int result = Double.compare(num1,num3);
+        int result = Double.compare(num1, num3);
         if (result == 0)
             System.out.println("Your numbers is equals");
         else if (result == 1)
             System.out.println("Max is " + num1);
         else
             System.out.println("Max is " + num3);
-        //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double (17)
+        //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double
         double result2 = Math.min(num1, num3);
         if (num1 != num3)
             System.out.println("Min is " + result2);
