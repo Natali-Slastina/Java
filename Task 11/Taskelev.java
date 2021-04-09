6a0418d3359debbf8d069301d952f39982b89e6c
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-09 22:28:52 +0300

    Update Taskelev.java

--- Task 11/Taskelev.java
+++ Task 11/Taskelev.java
@@ -6,22 +6,22 @@ public class Taskelev {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //Ввести первое  число с клавиатуры и записать его в строковую переменную.
-        System.out.println("Enter your first value as x.xxx if your value is double");
+        System.out.println("Enter your first value");
         String str = scan.nextLine();
-        //Ввести второе число с клавиатуры 
+        Double num1 = Double.valueOf(str);
+        //Ввести второе число с клавиатуры и сохранить его в целочисленную переменную типа int.
         System.out.println("Enter your second value");
-        Double num1 = scan.nextDouble();
+        Integer num2 = scan.nextInt();
         //Сравнить 2 числа и вывести большее на экран
-        Double num2 = Double.valueOf(str);
-        int result = num1.compareTo(num2);
+        double num3 = num2;
+        int result = Double.compare(num3,num1);
         if (result == 0)
             System.out.println("Your numbers is equals");
         else if (result == 1)
-            System.out.println("Max is " + num1);
+            System.out.println("Max is " + num3);
         else
-            System.out.println("Max is " + num2);
+            System.out.println("Max is " + num1);
         //Выведите также меньшее число на экран, предварительно переконвертировать его в тип double
-        double num3 = num2;
         double result2 = Math.min(num1, num3);
         if (num1 != num3)
             System.out.println("Min is " + result2);
