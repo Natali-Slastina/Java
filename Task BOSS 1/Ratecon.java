0324234cc4ca3fc6040610c35fafbfd4df1581a5
Natali-Slastina <40150015+Natali-Slastina@users.noreply.github.com>
2021-04-14 10:32:43 +0300

    Update Ratecon.java

--- Task BOSS 1/Ratecon.java
+++ Task BOSS 1/Ratecon.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Ratecon {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your cash in RUB");
-        double cash = scan.nextDouble();
         System.out.println("Enter dollar rate");
         double rate = scan.nextDouble();
+        System.out.println("Enter your cash in RUB");
+        double cash = scan.nextDouble();
         if (cash == 0 || rate == 0)
             System.out.println("You enter value = 0");
         else if (cash < 0 || rate < 0)
             System.out.println("You enter value < 0");
         else {
-            double money = cash * rate;
-            System.out.printf("Your money is " + "%.2f", money);
+            double money = cash / rate;
+            System.out.printf("Your money  is $" + "%.2f", money);
         }
     }
 }
